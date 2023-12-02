586d40ed1131aa17ef316abd359620af9a8e12fa
ZHAlipov <zhalipov@gmail.com>
2023-12-02 19:58:02 +0600

    upload to github

---
+++ src/moduleProject/CinemaSystem.java
@@ -0,0 +1,116 @@
+package moduleProject;
+
+import java.util.Scanner;
+
+public class CinemaSystem {
+    
+    private static char[][] cinemaHall;
+    private static int rows = 0;
+    private static int seats = 0;
+    private static int nMenuNumber = 1;
+    private static int soldTickets = 0;
+    
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        //stage1();
+        //stage1V2();
+        //stage2();
+        //stage3();
+        //stage4-5
+        initializeCinemaHall();
+        printSeats();
+        menu();
+    }
+
+    public static void initializeCinemaHall() {
+        System.out.println("Enter rows number: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter seats number: ");
+        seats = scanner.nextInt();
+
+        cinemaHall = new char[rows][seats];
+
+        System.out.println("Инициализация кинозала");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seats; j++) {
+                cinemaHall[i][j] = 'S';
+            }
+        }
+    }
+
+    public static void printSeats() {
+
+
+        //Печатаем номера мест
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for (int m = 1; m <= seats; m++) {
+            System.out.print(m + " ");
+        }
+        System.out.println();
+
+        //Печатаем ряды
+        for (int i = 0; i < rows; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < seats; j++) {
+                System.out.print(cinemaHall[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void menu() {
+        nMenuNumber = 1;
+
+        while (nMenuNumber != 0) {
+            System.out.println("\n1. Print seats\n2. Buy a ticket\n3. Show statistics\n0. Exit");
+            nMenuNumber = scanner.nextInt();
+            if (nMenuNumber == 1) {
+                printSeats();
+            } else if (nMenuNumber == 2) {
+                buyTicket();
+            } else if (nMenuNumber == 3) {
+                statistics();
+            } else {
+                System.out.println("Введено неверное значение!");
+            }
+        }
+    }
+
+    static void buyTicket() {
+        System.out.println("Покупка билета.\n Введите ряд: ");
+        int nBuyRow = scanner.nextInt();
+        System.out.println("Введите место: ");
+        int nBuySeat = scanner.nextInt();
+
+        if (nBuyRow-1 > rows || nBuySeat-1 > seats) {
+            System.out.println("Such seat doesn’t exist");
+        } else if (cinemaHall[nBuyRow - 1][nBuySeat - 1] == 'S') {
+            cinemaHall[nBuyRow - 1][nBuySeat - 1] = 'B';
+            System.out.println("Price: 10$");
+            soldTickets += 10;
+        } else if (cinemaHall[nBuyRow - 1][nBuySeat - 1] == 'B') {
+            System.out.println("This seat is already booked!");
+        }
+    }
+
+    static void statistics() {
+        int numFreeSeats = 0;
+        int numTotalSeats = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seats; j++) {
+                if (cinemaHall[i][j] == 'S') {
+                    numFreeSeats ++;
+                }
+                numTotalSeats ++;
+            }
+        }
+
+        System.out.printf("Current income: %d $%n", soldTickets);
+        System.out.printf("Total income: %d $%n", (rows*seats)*10);
+        System.out.printf("Number of free seats: %d $%n", numFreeSeats);
+        System.out.printf("Total number of seats: %d $%n", numTotalSeats);
+    }
+}
