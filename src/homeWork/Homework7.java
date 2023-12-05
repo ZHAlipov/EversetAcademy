1dd97aaa928fb15ead74ca21e3ef344781d99a39
ZHAlipov <zhalipov@gmail.com>
2023-12-05 19:19:20 +0600

    add tasks (max task 14)

--- src/homeWork/Homework7.java
+++ src/homeWork/Homework7.java
@@ -1,18 +1,29 @@
 package homeWork;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Homework7 {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
       //  printArray(selectionSort(initializeArray()));
-        printArray(descendingSort(initializeArray()));
+      //  printArray(descendingSort(initializeArray()));
       //  task1();
       //  task2();
       //  task3();
       //  task4();
       //  task5();
+      //  task6();
+      //  task7();
+      //  task8();
+      //  task9();
+      //  task10();
+      //  task11();
+      //  task12();
+      //  task13();
+        task14();
     }
 
     static int[] initializeArray() {
@@ -89,6 +100,15 @@ public class Homework7 {
         System.out.println("Sum values of Array = " + sum);
     }
 
+    static int[] cubicValues(int arr[]) {
+        int[] array = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            array[i] = arr[i] * arr[i] * arr[i];
+        }
+        return array;
+    }
+
 //    Exercise 1: Write a program to store elements in an array and print it
 //    Sample Output
 //    Array Size = 5
@@ -163,49 +183,140 @@ public class Homework7 {
     static void task6() {
 //    Exercise 6: Write a program to array elements to print cubic values
 //    Sample Output
-//    Array = {1, 2, 3, 4, 5}
+        int[] array = {1, 2, 3, 4, 5};
 //    Cubic Array Elements =1 8 27 64 125
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d", array[i] * array[i] * array[i]);
+            if (i < array.length-1) {
+                System.out.print(", ");
+            } else {
+                System.out.println(".");
+            }
+        }
     }
 
-}
 
-
-
-
+    static void task7() {
 //    Exercise 7: Write a program to find the maximum and minimum element in an array
 //    Sample Output
-//    Array = {23, 4, 32, 5, 75}
+        int array[] = {23, 4, 32, 5, 75};
 //    Maximum Element of Array = 75
 //    Minimum Element of Array = 4
+        Arrays.sort(array);
+        int minArr = array[0];
+        int maxArr = array[array.length-1];
+        System.out.println(minArr + " " + maxArr);
+    }
+
+
+    static void task8() {
 //    Exercise 8: Write a program to array elements print all Odd number
 //    Sample Output
 //    Array = {23, 45, 67, 34, 78}
 //    Odd Array Elements = 23 45 67
+        int[] arr = {23, 45, 67, 34, 78};
+
+        System.out.print("Odd Array Elements = ");
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] % 2) != 0) {
+                System.out.printf("%d ", arr[i]);
+            }
+        }
+        }
+
+    static void task9() {
 //    Exercise 9: Write a program to array elements print all Even number
 //    Sample Output
 //    Array = {23, 45, 89, 34, 12}
 //    Even Array Elements = 34 12
+        int[] arr = {23, 45, 89, 34, 12};
+
+        System.out.print("Even Array Elements = ");
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] % 2) == 0) {
+                System.out.printf("%d ", arr[i]);
+            }
+
+        }
+    }
+
+    static void task10() {
 //    Exercise 10: Write a program to array elements to print sum of Odd Numbers
 //    Sample Output
 //    Array = {12, 34, 59, 45, 22}
 //    Sum of Odd Array Elements = 104
+        int arr[] = {12, 34, 59, 45, 22};
+        int sumOddElements = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] % 2) != 0) {
+                sumOddElements += arr[i];
+            }
+        }
+        System.out.println(sumOddElements);
+    }
+
+    static void task11() {
 //    Exercise 11: Write a program to array elements to print sum of Even Numbers
 //    Sample Output
 //    Array = {56, 78, 45, 79, 34}
 //    Sum of Even Array Elements = 168
+        int arr[] = {56, 78, 45, 79, 34};
+        int sumEvenElements = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if((arr[i] % 2) == 0) {
+                sumEvenElements += arr[i];
+            }
+        }
+        System.out.println(sumEvenElements);
+    }
+
+    static void task12() {
 //    Exercise 12: Write a program to array elements to print sum of Cubic Values
 //    Sample Output
 //    Array = {1, 2, 3, 4, 5}
 //    Sum of Cubic Array Elements = 225
+        int array[] = {1, 2, 3, 4, 5};
+        printArray(array);
+        printArray(cubicValues(array));
+        sumArray(cubicValues(array));
+    }
+
+    static void task13() {
 //    Exercise 13: Write a program in to copy the elements of one array into another array
 //    Sample Output
 //    Array = {1, 2, 3, 4, 5}
 //    Copy Array Elements one to Another Array = {1, 2, 3, 4, 5}
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = new int[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            array2[i] = array1[i];
+        }
+        System.out.println("Array = " + Arrays.toString(array1));
+        System.out.println("Copy Array Elements one to Another Array = " + Arrays.toString(array2));
+    }
+
+    static void task14() {
 //    Exercise 14: Write a program to merge two arrays elements to store third array
 //    Sample Output
 //    First Array = {1, 2, 3, 4, 5}
 //    First Array = {6, 7, 8, 9, 10}
 //    Merge two Array Elements = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {6, 7, 8, 9, 10};
+        int[] array3 = new int[array1.length + array2.length];
+
+        for (int i = 0; i < array2.length; i++) {
+            array3[i] = array1[i];
+        }
+        for (int i = array2.length; i < array3.length; i++) {
+            array3[i] = array2[i-array2.length];
+        }
+
+        System.out.println(Arrays.toString(array3));
+    }
 //    Exercise 15: Write a program to array elements print all Positive number
 //    Sample Output
 //    Array = {67, -4, 3, -5, 44}
@@ -264,5 +375,5 @@ public class Homework7 {
 //    Array = {1, 2, 4, 5, 6, 7}
 //    Missing Array Number = 3
 
-
+}
 
