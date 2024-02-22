9527bb6f7da25dfb39259a11e9f6fbaef496280a
Zhassulan <Zhalipov@gmail.com>
2024-02-22 21:05:34 +0600

    add task 16

--- src/Module3/HomeWork/ArrayList/Ex1/Task16.java
+++ src/Module3/HomeWork/ArrayList/Ex1/Task16.java
@@ -1,6 +1,7 @@
 package Module3.HomeWork.ArrayList.Ex1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Task16 {
     // Напишите программу на Java для клонирования списка массивов в другой список массивов.
@@ -10,7 +11,10 @@ public class Task16 {
         numbers.add(1);
         numbers.add(2);
 
-        ArrayList<Integer> numbers1 = new ArrayList<>(numbers);
+        ArrayList<Integer> numbers1 = (ArrayList<Integer>) numbers.clone();
+
+        numbers1.remove(0);
+        System.out.println(numbers);
         System.out.println(numbers1);
         System.out.println(numbers.hashCode());
         System.out.println(numbers1.hashCode());
